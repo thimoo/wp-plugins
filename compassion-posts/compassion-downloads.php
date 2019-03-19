616af8bd6254e018629d293239fd6d318f26447c
Christopher Meier <25900023+c-meier@users.noreply.github.com>
2019-03-19 12:25:36 +0100

    WP-28 : Show the custom post types agenda and download in the REST API.

--- compassion-posts/compassion-downloads.php
+++ compassion-posts/compassion-downloads.php
@@ -50,6 +50,7 @@ class CompassionDownloads
             'menu_position'         => 5,
             'show_in_admin_bar'     => true,
             'show_in_nav_menus'     => true,
+            'show_in_rest'          => true,
             'can_export'            => true,
             'has_archive'           => false,
             'exclude_from_search'   => true,
