6e6e708de3677ab6e79885effecee7ef2b421aa6
olivier requet <orequet@compassion.ch>
2020-08-24 14:45:31 +0200

    fpdf has new version and removed obsolet function :get_magic_quotes_runtime

--- compassion-letters/vendor/setasign/fpdf/tutorial/tuto7.php
+++ compassion-letters/vendor/setasign/fpdf/tutorial/tuto7.php
@@ -6,6 +6,6 @@ $pdf = new FPDF();
 $pdf->AddFont('Calligrapher','','calligra.php');
 $pdf->AddPage();
 $pdf->SetFont('Calligrapher','',35);
-$pdf->Cell(0,10,'Enjoy new fonts with FPDF!');
+$pdf->Cell(0,10,'Changez de police avec FPDF !');
 $pdf->Output();
 ?>
