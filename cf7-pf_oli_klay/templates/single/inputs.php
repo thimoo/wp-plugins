6cbb51022bf63e0818949593b5a1d95f38cb9004
Emanuel Cino <ecino@compassion.ch>
2021-09-21 15:52:43 +0200

    FIX default fund amount in shortcodes

--- cf7-pf_oli_klay/templates/single/inputs.php
+++ cf7-pf_oli_klay/templates/single/inputs.php
@@ -3,7 +3,7 @@
         <label class="text-left middle"><?php _e('Betrag deiner Spende in CHF', 'donation-form'); ?></label>
     </div>
     <div class="small-12 medium-8 columns">
-        <input type="text" placeholder="<?php _e('bitte nur Zahlen', 'donation-form'); ?>" required data-msg="<?php _e('Betrag erforderlich', 'donation-form'); ?>" class="input-field" name="wert" value="<?php echo (isset($session_data['wert'])) ? $session_data['wert'] : ''; ?>">
+        <input type="text" placeholder="<?php _e('bitte nur Zahlen', 'donation-form'); ?>" required data-msg="<?php _e('Betrag erforderlich', 'donation-form'); ?>" class="input-field" name="wert" value="<?php echo $_SESSION["fund_amount"]?>">
     </div>
 </div>
 <div class="row">
