fd222d130481d4ed5fdabe6fec8d4e9d97099e9a
olivier compassion <orequet@compassion.ch>
2024-07-29 12:11:25 +0200

    remove donation amounts

--- cf7-pf_oli_klay/templates/vocational_KE/inputs.php
+++ cf7-pf_oli_klay/templates/vocational_KE/inputs.php
@@ -7,7 +7,7 @@
 <!--</div>-->
 <div class="row don_unique"> <!--  style="display: none;">-->
     <!--donation amounts>-->
-       <!--<div class="small-12 medium-12 columns">
+      <div class="small-12 medium-12 columns">
            <div class="row">
                <div class="small-12 medium-6 columns">
                    <button class="button button-beige buttondonation" value="40" >40.- CHF</button>
@@ -23,11 +23,11 @@
                <div class="small-12 medium-6 columns">
                    <button class="button button-beige buttondonation" value="5000" >5000.- CHF</button>
                </div>
-           </div>-->
+           </div>
         <div class="row marg-top-10">
             <div class="small-12 columns ">
                 <input id="wert" type="text" step="0.01" required placeholder="<? echo __('Wähle einen Betrag oder gib einen freien Betrag', 'donation-form') ?>" class="input-field buttondonationvalue" name="wert" value="<?php echo $_SESSION["fund_amount"] ?? '' ?>">
             </div>
         </div>
- <!--   </div>-->
+ </div>
 </div>
